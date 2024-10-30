f90815eb0d397509fbb4bcdf5a9e13d6f6385e73
David Martin Sørensen <daso@itu.dk>
2024-10-30 17:37:48 +0100

    Feat : Made WordGraph able to initialize

--- src/Gallery_of_Graphs/word_graph.py
+++ src/Gallery_of_Graphs/word_graph.py
@@ -6,7 +6,7 @@ from Gallery_of_Graphs.graph import Graph
     For instance ``begin'' and ``binge'' are neighbours, and so are ``turns'' and ``terns'' for $k=1$.
 
     The word graphs  come in two flavours.
-    The \emph{rusty word} graphs are guaranteed to include ``begin,'' ``ender,'' and ``rusty.''
+    The *rusty word* graphs are guaranteed to include ``begin,'' ``ender,'' and ``rusty.''
     The vertex corresponding to ``rusty'' is coloured red, no other vertices are red.
 
     The filenames are "rusty-$k$-$n$".
@@ -14,4 +14,16 @@ from Gallery_of_Graphs.graph import Graph
 
 class WordGraph(Graph):
     def __init__(self, input_lines : list[str]):
+        self.node_dict : dict[str, int] = {}
+        self.node_dict_size = 0
         super().__init__(input_lines)
+    
+    def node_to_int(self, s : str) -> tuple[int, int]:
+        cached = self.node_dict.get(s)
+        if cached is not None:
+            return cached
+        else:
+            to_be_cached = self.node_dict_size
+            self.node_dict[s] = to_be_cached
+            self.node_dict_size += 1
+            return to_be_cached
