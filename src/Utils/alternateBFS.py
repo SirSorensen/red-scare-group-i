ac1b11f2b9168f17d0ec0dd4aa398c7ed4f1aee4
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2024-11-04 09:55:55 +0100

    changed import - because of circular import error - to include IGraph interface, instead of Graph

--- src/Utils/alternateBFS.py
+++ src/Utils/alternateBFS.py
@@ -1,15 +1,15 @@
-from Gallery_of_Graphs.graph import Graph
+from Gallery_of_Graphs.graph_interface import IGraph
 from queue import Queue
 
 class AlternateBreadthFirstPaths:
-    def __init__(self, G : Graph):
+    def __init__(self, G : IGraph):
         self.marked : list[bool] = [False]*G.node_amount
         self.edgeTo : list[int] = [0]*G.node_amount
         self.s : int = G.start
         self.bfs(G, self.s)
     
     
-    def bfs(self, G : Graph, s : int):
+    def bfs(self, G : IGraph, s : int):
         queue = Queue()
         self.marked[s] = True
         queue.put(s)
