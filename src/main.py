a1c7be8792144b957ee0216c4e3aaf2a2a23c125
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2024-11-20 09:08:46 +0100

    Format : Removed redundant import and made a comment more readable

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from Utils.graph_factory import *
-from Gallery_of_Graphs.graph import Graph
 from Utils.result_printer import *
 
 # uses 'G-ex' as default file name if no argument is given
 file_name = 'G-ex' if len(sys.argv) < 2 else sys.argv[1]
-
 print_solution(file_name)
 
 
-# Outcomment if you want to print out all results:
+### Outcomment if you want to print out all results: ###
 #print_all_solution(print_to_file = True)
 #print_latex_table(print_to_file = True)
