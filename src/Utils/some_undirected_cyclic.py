2e9c0c65138eeb0316f206881aaf0ff81db0fd45
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2024-11-21 16:53:59 +0100

    added graphfiles

---
+++ src/Utils/some_undirected_cyclic.py
@@ -0,0 +1,26 @@
+def vertex_disjoint_paths(graph):
+    from collections import deque
+    start = graph.start
+    end = graph.end
+    red_vertices = graph.node_colours
+
+    def bfs_paths(source, target):
+        has_red = red_vertices[source]
+        queue = deque([(has_red, source, [source])])
+
+        while queue:
+            (has_red, vertex, path) = queue.popleft()
+            
+            for neighbor in graph.edges[vertex]:
+                if neighbor not in path:
+                    has_red = has_red or red_vertices[neighbor]
+                    new_path = path + [neighbor]
+
+                    if neighbor == target:
+                        if has_red:
+                            return True
+                    else:
+                        queue.append((has_red, neighbor, new_path))
+        return False
+
+    return bfs_paths(start, end)
