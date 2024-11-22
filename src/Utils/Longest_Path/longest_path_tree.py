ea000110c4014dcb3689617df0d5fcf9023febe7
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2024-11-22 14:30:20 +0100

    Refactor : Removed redundant code - Longest_Path_Tree.build_tree_graph always makes a copy of edges, since we only make a longest_path_tree if the graph is undirected - Longest_Path_Tree.longest_path_tree therefore only called self.dfs, which could just be done in the constructor

--- src/Utils/Longest_Path/longest_path_tree.py
+++ src/Utils/Longest_Path/longest_path_tree.py
@@ -5,20 +5,8 @@ from Utils.Longest_Path.longest_path import Longest_Path
 class Longest_Path_Tree(Longest_Path):
     def __init__(self, g : IGraph):
         super().__init__(g)
-        self.longest_path_tree(g.node_colours, g.node_amount, g.edges)
+        self.dfs(0, -1, g.edges, g.node_colours)
 
-    def build_tree_graph(self, N : int, edges : list[list[int]]) -> list[int]:
-        tree_graph = [[] for _ in range(N)]
-        visited_edges = set()
-
-        for u in range(N):
-            for v in edges[u]:
-                if (u, v) not in visited_edges and (v, u) not in visited_edges:
-                    tree_graph[u].append(v)
-                    tree_graph[v].append(u)
-                    visited_edges.add((u, v))
-
-        return tree_graph
 
     def dfs(self, node : int, red_count : int, tree_graph : list[int], node_colours : list[bool]):
         self.visited[node] = True
@@ -27,7 +15,4 @@ class Longest_Path_Tree(Longest_Path):
         for neighbor in tree_graph[node]:
             if not self.visited[neighbor]:
                 self.dfs(neighbor, node, tree_graph, node_colours)
-
-    def longest_path_tree(self, node_colours : list[bool], N : int, edges : list[list[int]]):
-        tree_graph = self.build_tree_graph(N, edges)
-        self.dfs(0, -1, tree_graph, node_colours)
+        
