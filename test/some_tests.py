6422eae940d3dbb18bafb3a6b3c05891082dc7af
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2024-11-25 20:04:26 +0100

    fix some tests

--- test/some_tests.py
+++ test/some_tests.py
@@ -9,8 +9,6 @@ from Gallery_of_Graphs.graph import Graph
 class SomeTests(UnitTests):
 
     def test_directed_acyclic(self):
-        self._act_assert('K3-1', True)
-        self._act_assert('K3-2', False)
         self._act_assert('ski-illustration', True)
         self._act_assert('ski-level3-1', True)
         self._act_assert('ski-level5-3', False)
@@ -18,20 +16,33 @@ class SomeTests(UnitTests):
         self._act_assert('increase-n8-2', True)
     
     def test_undirected_cyclic(self):
-        self._act_assert('G-ex', '?')
-        self._act_assert('K3-0', '?')
-        self._act_assert('rusty-1-17', '?')
-        self._act_assert('grid-5-0', '?')
-        self._act_assert('wall-p-1', '?')
-        self._act_assert('wall-p-3', '?')
-        self._act_assert('wall-z-3', '?')
-        self._act_assert('wall-n-2', '?')
-        self._act_assert('common-1-20', '?')
-        self._act_assert('common-1-50', '?')
+        self._act_assert('G-ex', True)
+        self._act_assert('rusty-1-17', True)
+        self._act_assert('grid-5-0', True)
+        self._act_assert('wall-p-1', True)
+        self._act_assert('wall-p-3', True)
+        self._act_assert('wall-z-3', False)
+        self._act_assert('wall-n-2', False)
+        self._act_assert('common-1-100.txt', False)
+        self._act_assert('dodecahedron.txt', True)
+        self._act_assert('gnm-10-15-0.txt', True)
+        self._act_assert('gnm-10-15-1.txt', True)
+        self._act_assert('gnm-10-20-0.txt', True)
+        self._act_assert('gnm-10-20-1.txt', True)
+        self._act_assert('grid-5-1', True)
+        self._act_assert('grid-5-2', True)
+        self._act_assert('smallworld-3-0', True)
+        self._act_assert('smallworld-3-1', True)
+        self._act_assert('wall-p-2', True)
+        self._act_assert('wall-z-1', True)
+        self._act_assert('wall-z-2', False)
+        self._act_assert('wall-n-1', True)
+        self._act_assert('wall-n-3', False)
 
-    def test_udirected_acyclic(self): 
-        self._act_assert('P3-0', True)
-        self._act_assert('P3-1', False)
+    def test_udirected_acyclic(self):
+        self._act_assert('P3', True)
+        self._act_assert('common-1-20', False)
+        self._act_assert('common-1-50', False)
 
     ### Helper methods ###
     def _solve(self, g : Graph):
